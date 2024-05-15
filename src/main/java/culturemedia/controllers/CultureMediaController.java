fa695550078dc9ea49b098644cf3c3b96a19a39f
DanielHerrera22 <danielherrerahenao509@gmail.com>
2024-05-15 07:46:55 -0500

    Merge pull request #3 from DanielHerrera22/R3-Creating-Video-views-Structures
    Merge pull request #1 from DanielHerrera22/R1-Adding-Maven

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -1,26 +1,20 @@
 package culturemedia.controllers;
 
-import java.util.*;
+import java.util.List;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
+import culturemedia.service.CultureMediaService;
 import culturemedia.service.impl.CultureMediaServiceImpl;
 
 public class CultureMediaController {
-
-    private final CultureMediaServiceImpl cultureMediaService;
-
+    private final CultureMediaService cultureMediaService;
 
     public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
-        videos = cultureMediaService.findAll();
-        return videos;
+    public List<Video> findAllVideos() throws VideoNotFoundException {
+        return cultureMediaService.findAll();
     }
-
-
 }
